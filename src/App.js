b38661a90dbd3cdb8b8528adbc9b255c261e5f54
[jquinn24] <[jquinn24@charlotte.edu]>
2025-03-31 20:58:30 -0400

    First Commit JQ

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import React from 'react';
-
+import graph from './images/graph.png';
+import why from './images/why.png';
+import TaxGuide from './pages/TaxGuide';
+import Graph from './pages/Graph';
+import Community from './pages/Community'; // New import
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
+          <Routes>
+            <Route path="/" element={
+              <>
+                <img src={graph} alt="Graph" style={{ height: 'auto', maxHeight: '300px', width: 'auto' }} />
+                <img src={why} alt="Why" style={{ maxWidth: '45%', height: 'auto' }} />
+              </>
+            } />
+            <Route path="/tax-guide" element={<TaxGuide />} />
+            <Route path="/graph" element={<Graph />} />
+            <Route path="/community" element={<Community />} /> {/* New route */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,26 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer>
-            <div className='footer-logo'>
-                <img id='logo' src={logo} alt='logo'/>
-
+        <footer className="footer-container">
+            <div className="footer-logo">
+                <img id="logo" src={logo} alt="logo" />
             </div>
-            <div className='footer-links'>
+            <div className="footer-links-container">
+                <div className="footer-links">
+                    <ul>
+                        <li><a href="Community">Marketplace</a></li>
+                        <li><a href="NFTs">NFTs</a></li>
+                        <li><a href="Raffles">Raffles</a></li>
 
+                    </ul>
+                    <ul>
+                    <li><a href="About_Us">About Us</a></li>
+                        <li><a href="News">News</a></li>
+                        <li><a href="Tax_Guide">Tax Guide</a></li>
+                    </ul>
+                    <ul>
+                    </ul>
+                </div>
             </div>
         </footer>
     );
